cb190e14a22adeb727a30d3c97bf8d812bf4077e
salmanbinnaser <salmananu222@gmail.com>
2021-05-17 21:03:42 +0530

    chain.js adeed

--- sketch.js
+++ sketch.js
@@ -33,9 +33,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(760,120,150, PI);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body);
+   
 }
 
 function draw(){
@@ -58,7 +60,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
